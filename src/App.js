7e41725661220eddf9222190e7d55ae5b70bdaf0
chipo mapondera <chipo.mapondera@ynap.com>
2019-10-16 15:31:05 +0100

    NAP PLP minor edit made to App.js, removed styling that was not functional

--- src/App.js
+++ src/App.js
@@ -3,29 +3,18 @@ import ServiceMessage from './components/serviceMessage'
 import SiteHeader from './components/siteHeader'
 import NavBar from './components/navBar'
 import ProductPageElements from './components/productPageElements/ProductPageElements'
-import { createComponent } from 'react-fela'
 import data from './data/'
-
-const appBox = ()=>({
-  width: '100%',
-  display: 'block',
-  margin: '0 auto',
-  boxizing: 'border-box',
-})
-
-const AppBox = createComponent (appBox, 'div')  
 
 const App =()=> {
   return (
-    <AppBox>
+    <div>
       <ServiceMessage info={data.serviceMessage} />
       <SiteHeader info={data.siteHeader} />
       <NavBar info={data.navBar} />
       <ProductPageElements 
           productPageTitle ={data.productPageHeader}
           productPageInfo={data.productPageBody} />
-
-    </AppBox>
+    </div>
   );
 }
 
