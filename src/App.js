d2b6e751762d3e90c04ce9d1bb722987aebfddc8
chipo mapondera <chipo.mapondera@ynap.com>
2019-11-27 16:56:30 +0000

    fetching the Shoe category data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ServiceMessage from './components/serviceMessage'
 import SiteHeader from './components/siteHeader'
 import NavBar from './components/navBar'
@@ -6,6 +6,23 @@ import ProductPageElements from './components/productPageElements/ProductPageEle
 import data from './data/'
 
 const App =()=> {
+  const [productsData, setProductsData] = useState()
+
+  const grabProductsData = async() => {
+    const res = await fetch('https://api.net-a-porter.com/NAP/GB/en/categories')
+    const fetchData = await res.json()
+    return fetchData
+  }
+
+  useEffect(async() => {
+    const fetchedProducts = await grabProductsData()
+    setProductsData(fetchedProducts)
+    const filterData = fetchedProducts.categories
+    const shoeCategory = filterData.filter((category) => {
+      return category.id == 4 && category.name=='Shoes' && category.urlKey=='Shoes'
+    })
+  }, [])
+
   return (
     <div>
       <ServiceMessage info={data.serviceMessage} />
